edd36be9b1283232fc76d32fbffd876fa599b71c
Tolga <dfavenfre@protonmail.com>
2023-06-18 15:45:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ from datetime import datetime
 from typing import Iterable
 import pickle
 
-
-
 def main():
-
     # SIDEBAR CONTENT
     st.sidebar.title("Bank Customer Term Deposit Predictor")
     st.sidebar.write(
@@ -22,7 +19,6 @@ def main():
         
         """
     )
-    
     # Social Hubs
     st.sidebar.markdown(
 
@@ -58,34 +54,40 @@ def main():
         time.sleep(1)
         load_message.empty()
 
-
     # MODEL CONTENT
-    
     ## Job Feature
     occupation_values ={"Administrative": 0,"Blue-collar": 1,"Entrepreneur": 2,"Housemaid": 3,
                         "Management": 4,"Retired": 5,"Self-employed": 6,"Services": 7,
                         "Student": 8,"Technician": 9,"Unemployed": 10}
-    
     select_job = st.selectbox("Type of job", list(occupation_values.keys()))
     if select_job:        
         selected_job = occupation_values[select_job]
-
     ## Day of Week
     day_values = {"Monday":0,"Tuesday":1,"Wednesday":2,"Thursday":3,"Friday":4,"Saturday":5,"Sunday":6,}
     select_day = st.selectbox("Last contact day of the week", list(day_values.keys()))
     if select_day:
         selected_day = day_values[select_day]
-
     ## Campaign
-
-    ## poutcome 
-
+    campaign = st.number_input('Number of contacts performed during this campaign and for this client')
     ## poutcome
-
+    list_outcome = {"failure":0, "non-existed":1,"success":2}
+    p_outcome = st.selectbox("Outcome of the previous marketing campaign", list(list_outcome.keys()))
+    if p_outcome:
+         selected_poutcome = list_outcome[p_outcome]
     ## cons.price.idx
-
+    cpi = st.number_input("Consumer price index - monthly indicator")
     ## euribor3m
-
+    euribor = st.number_input("Euribor 3 month rate - daily indicator")
+    # Prediction
+    prediction_button = st.button(label="Predict")
+    X_list = [selected_job, selected_day, campaign, selected_poutcome, cpi, euribor]
+    X_array = np.array(X_list).reshape(1, -1)
+    pred_df = pd.DataFrame(X_array, columns=['job', 'day_of_week', 'campaign', 'poutcome', 'cons.price.idx', 'euribor3m'])
+    if prediction_button:
+        pred = xgbclassifier.predict(pred_df)
+        if pred ==0 :
+             st.write(f"Prediction Outcome: {pred[0]} [{'No'}]")
+        else:
+             st.write(f"Prediction Outcome: {pred[0]} \nTendency Likelyhood: {'Yes'}")
 if __name__ == '__main__':
 	main()
-
